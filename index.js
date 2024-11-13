c1e968e90d8dc715c0bd7d82b731a13f595d3d8c
DongxiaoLi98 <darialddx@gmail.com>
2024-11-13 15:41:01 -0500

    a5Lab

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.json());
+
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000)
