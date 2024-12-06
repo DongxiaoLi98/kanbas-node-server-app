f650e4085b637e4316aefa49aaf9fe77390f5a42
DongxiaoLi98 <darialddx@gmail.com>
2024-12-05 20:58:55 -0500

    a6_retrive_enroll

--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -3,10 +3,7 @@ export async function findCoursesForUser(userId) {
     const enrollments = await model.find({ user: userId }).populate("course");
     return enrollments.map((enrollment) => enrollment.course);
    }
-export async function findUsersForCourse(courseId) {
-    const enrollments = await model.find({ course: courseId }).populate("user");
-    return enrollments.map((enrollment) => enrollment.user);
-   }
+
 export function enrollUserInCourse(user, course) {
     return model.create({ user, course });
    }
@@ -15,3 +12,7 @@ export function unenrollUserFromCourse(user, course) {
     return model.deleteOne({ user, course });
    }
    
+export async function findUsersForCourse(courseId) {
+    const enrollments = await model.find({ course: courseId }).populate("user");
+    return enrollments.map((enrollment) => enrollment.user);
+   }
